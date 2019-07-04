9a513e79628915921319b57049c43a9fa3154115
Jebarcha <jebarcha@hotmail.com>
2019-07-04 13:19:09 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+	{ path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
+	// { path: 'agregar', loadChildren: './pages/agregar/agregar.module#AgregarPageModule' }
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }) ],
+	exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
